5dc742e5798a2cb7e6ae5d15657118f4fd8ad12d
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-13 16:13:45 -0300

    added button class variable to store classes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,19 @@
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
+  const buttonClasses =
+    "m-4 bg-emerald-200 hover:bg-blue-700 text-slate-700 font-bold py-2 px-4 rounded-full";
+
   return (
     <nav>
       <div className="container navbar h-20 flex justify-center gap-10 items-center">
-        <NavLink to="/" className="m-4">
+        <NavLink to="/" className={buttonClasses}>
           Home
         </NavLink>
-        <NavLink to="/projects" className="m-4">
+        <NavLink to="/projects" className={buttonClasses}>
           Projects
         </NavLink>
-        <NavLink to="/about" className="m-4">
+        <NavLink to="/about" className={buttonClasses}>
           About
         </NavLink>
       </div>
