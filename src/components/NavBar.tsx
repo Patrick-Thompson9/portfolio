331d513fc5866349947ae61b2e1f01557158588e
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-13 16:07:15 -0300

    changed text to white

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,18 @@
-//import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
     <nav>
       <div className="container navbar h-20 flex justify-center gap-10 items-center">
-        <div className="m-4">Home</div>
-        <div className="m-4">Projects</div>
-        <div className="m-4">About</div>
+        <NavLink to="/" className="m-4">
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="m-4">
+          Projects
+        </NavLink>
+        <NavLink to="/about" className="m-4">
+          About
+        </NavLink>
       </div>
       <div className="navSpacer waves"></div>
     </nav>
