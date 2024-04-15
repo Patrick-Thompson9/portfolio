5dbbf47dcd7f629a044bb2822e3596fc675fa54c
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-15 00:09:58 -0300

    waves are animated in navbar component but with some bugs

---
+++ src/assets/animation/WaveAnimation.js
@@ -0,0 +1,30 @@
+const animateWave1 = KUTE.fromTo(
+  "#wave11",
+  { path: "#wave11" },
+  { path: "#wave21" },
+  { repeat: 999, duration: 3000, yoyo: true }
+).start();
+const animateWave2 = KUTE.fromTo(
+  "#wave12",
+  { path: "#wave12" },
+  { path: "#wave22" },
+  { repeat: 999, duration: 3000, yoyo: true }
+).start();
+const animateWave3 = KUTE.fromTo(
+  "#wave13",
+  { path: "#wave13" },
+  { path: "#wave23" },
+  { repeat: 999, duration: 3000, yoyo: true }
+).start();
+const animateWave4 = KUTE.fromTo(
+  "#wave14",
+  { path: "#wave14" },
+  { path: "#wave24" },
+  { repeat: 999, duration: 3000, yoyo: true }
+).start();
+const animateWave5 = KUTE.fromTo(
+  "#wave15",
+  { path: "#wave15" },
+  { path: "#wave25" },
+  { repeat: 999, duration: 3000, yoyo: true }
+).start();
