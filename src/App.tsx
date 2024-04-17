9fa77c13fed316ec4b12fe64b35f0921fd8316f9
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-16 22:55:06 -0300

    Changed router to dodge routes error, added context to send props to grandchild project component, made gradient following mouse on link svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsPage";
 import MainLayout from "./layouts/MainLayout";
+import React, { useRef, useState } from "react";
+
+type AppContextType = {
+  cardsRef: React.RefObject<HTMLElement>;
+  handleMouseMove: (e: React.MouseEvent<Element, MouseEvent>) => void;
+  cursorPos: { x: number; y: number };
+};
+
+const AppContext = React.createContext<AppContextType | null>(null);
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Route>
-    )
+  const cardsRef = useRef<HTMLElement>(null);
+  const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (cardsRef.current !== null) {
+      const rect = cardsRef.current.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      setCursorPos({ x: x, y: y });
+    }
+  };
+
+  return (
+    <AppContext.Provider value={{ cursorPos, cardsRef, handleMouseMove }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="projects" element={<ProjectsPage />} />
+            <Route path="about" element={<AboutPage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </AppContext.Provider>
   );
-  return <RouterProvider router={router} />;
 }
 
 export default App;
+export { AppContext };
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,17 +1,41 @@
 import { NavLink } from "react-router-dom";
 import gitHubLogo from "../assets/logos/github-logo.svg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { useContext, useState, useEffect } from "react";
+import { AppContext } from "../App";
 
 interface buttonClasses {
   buttonClasses: string;
 }
 
 function Project({ buttonClasses }: buttonClasses) {
+  const context = useContext(AppContext);
+
+  if (!context) {
+    return null;
+  }
+
+  const { cursorPos, cardsRef, handleMouseMove } = context;
+  const [gradientCenter, setGradientCenter] = useState({
+    cx: "50%",
+    cy: "50%",
+  });
+
+  useEffect(() => {
+    if (cardsRef.current !== null) {
+      const rect = cardsRef.current.getBoundingClientRect();
+      setGradientCenter({
+        cx: `${(cursorPos.x / rect.width) * 100}%`,
+        cy: `${(cursorPos.y / rect.height) * 100}%`,
+      });
+    }
+  }, [cursorPos, cardsRef]);
+
   return (
     <section
+      ref={cardsRef}
+      onMouseMove={(e) => handleMouseMove(e)}
       className="lg:w-2/3 lg:mx-auto shadow-lg transition duration-1000 border-2 border-neutral-300 bg-slate-200 text-black p-10 rounded-lg
-     stroke-[0.1] hover:stroke-[0.2] dark:bg-slate-600 dark:text-white dark:border-neutral-600"
+     stroke-[10] hover:stroke-[20] stroke-red-500 dark:bg-slate-600 dark:text-white dark:border-neutral-600"
     >
       <div className="flex flex-col items-center justify-center">
         <span>Project Name</span>
@@ -34,13 +58,25 @@ function Project({ buttonClasses }: buttonClasses) {
                 focusable="false"
                 data-prefix="fas"
                 data-icon="up-right-from-square"
-                className="svg-inline--fa fa-up-right-from-square size-7"
+                className="svg-inline--fa fa-up-right-from-square size-7 transition duration-200"
                 role="img"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
               >
+                <defs>
+                  <radialGradient
+                    id="linkGradient"
+                    gradientUnits="userSpaceOnUse"
+                    cx={gradientCenter.cx}
+                    cy={gradientCenter.cy}
+                  >
+                    <stop stopColor="#f7f7f7" />
+                    <stop offset="1" stopColor="#10b981" />
+                  </radialGradient>
+                </defs>
                 <path
                   fill="currentColor"
+                  stroke="url(#linkGradient)"
                   d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V32c0-17.7-14.3-32-32-32H352zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"
                 ></path>
               </svg>
