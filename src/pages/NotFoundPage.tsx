02da04462f00f24c9953b479c1544642b6c98e44
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-17 20:12:39 -0300

    changed go back to button classes

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { buttonClasses } from "../layouts/MainLayout";
 
 function NotFoundPage() {
   return (
@@ -11,10 +12,7 @@ function NotFoundPage() {
       />
       <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
       <p className="text-xl mb-5">This page does not exist</p>
-      <Link
-        to="/"
-        className="text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4"
-      >
+      <Link to="/" className={buttonClasses}>
         Go Back
       </Link>
     </section>
