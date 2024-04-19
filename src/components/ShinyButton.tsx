72ca2e599b4ad147541126e48cb6abfa96cd2244
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-19 00:23:16 -0300

    added framer motion to make a shiny button component

--- src/components/ShinyButton.tsx
+++ src/components/ShinyButton.tsx
@@ -1,7 +1,35 @@
 import { buttonClasses } from "../layouts/MainLayout";
+import { motion } from "framer-motion";
 
 function ShinyButton() {
-  return <button className={buttonClasses}>Shiny Button</button>;
+  return (
+    <motion.button
+      className="m-10 py-2 px-4 bg-black rounded-md button-radial-gradient"
+      onClick={() => console.log("Shiny Button Clicked!")}
+      initial={{ "--x": "100%" }}
+      animate={{ "--x": "-100%" }}
+      whileTap={{ scale: 0.5 }}
+      transition={{
+        repeat: Infinity,
+        repeatType: "loop",
+        repeatDelay: 2,
+        type: "spring",
+        stiffness: 30,
+        damping: 20,
+        mass: 0.5,
+        scale: {
+          type: "spring",
+          stiffness: 1,
+          damping: 5,
+          mass: 0.1,
+        },
+      }}
+    >
+      <span className="text-slate-100 tracking-wide font-light size-full block relative linear-mask">
+        Shiny Button
+      </span>
+    </motion.button>
+  );
 }
 
 export default ShinyButton;
